57d26dff5ab8c38641649e85dcfcb7e978442255
ATyagi-LU <a.tyagi2@lancaster.ac.uk>
2023-11-04 15:32:11 +0000

    Added all resources and files. Created Tests.java

---
+++ Resources/Tests.java
@@ -0,0 +1,14 @@
+public class Tests implements CollectionTest {
+
+    public Tests(){
+        
+    }
+
+    public void setSize(int size) {
+
+    }
+
+    public void runTest(CollectionTest.CollectionType type, CollectionTest.TestType test, int iterations) {
+
+    }
+}
